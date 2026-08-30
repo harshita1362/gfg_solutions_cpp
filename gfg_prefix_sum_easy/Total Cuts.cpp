6915d9469442e0bb37f93b23253e738dc13b7452
harshita1362 <147728364+harshita1362@users.noreply.github.com>
2026-08-30 04:26:28 -0700

    Create Total Cuts.cpp

---
+++ gfg_prefix_sum_easy/Total Cuts.cpp
@@ -0,0 +1,5 @@
+/* Total Cuts
+You are given an array a[] of n integers and an integer k, find the total number of cuts that you can make such that for each cut these two conditions are satisfied
+- A cut divides into two parts of non-zero lengths. 
+- Sum of the largest in the left part and the smallest in the right part is greater than or equal to k.
+*/
